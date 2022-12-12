b1a3cc865e38e3d2217f7ec92e4279e9fb889afb
Jerry Jian <emisjerry@gmail.com>
2022-12-12 10:19:27 +0800

    zh-TW 繁體中文語言包

--- src/lang/locale/zh-tw.ts
+++ src/lang/locale/zh-tw.ts
@@ -1,3 +1,53 @@
 // 繁體中文
 
-export default {}
+export default {
+  // main.ts
+  broken: 'Emo工作異常，檢視你的目標平台選擇',
+  donate: '讚賞',
+  'donate desc': '哈？這是我出於樂趣而製作的開源外掛。它是完全免費的。然而，如果你真的因為那麼喜歡就一定要寄錢給我，那就隨便往我帽子里扔些硬幣吧。謝謝。',
+  'upload error': '上傳失敗',
+  'parms error': '目前平台所需必要參數未填寫完全',
+  // settings-tab.ts
+  'target hosting': '託管平台',
+  'target hosting desc': '選擇目標。上傳之前，請確保所選平台的必要參數已填寫完成。',
+  // fragment-cloudinary.ts
+  'Cloudinary Settings': 'Cloudinary配置',
+  'Upload Folder desc': '要在Cloudinary中使用的資料夾名稱。注意，如果在Cloudinary預設了資料夾，則會忽略此項。',
+  // fragment-github.ts
+  'Github Settings': 'Github配置',
+  owner: '使用者名稱',
+  repo: '儲存庫',
+  branch: '分支',
+  token: '令牌',
+  message: '提交資訊',
+  path: '路徑',
+  'random filename': '隨機檔名',
+  'random filename desc': '採用隨機檔名將極大避免重名。如果確定在上傳過程中需要使用原檔名，請確保所選路徑下不會出現重名問題。',
+  // fragment-imgbb.ts
+  'imgbb Settings': 'imgbb配置',
+  // fragment-imgur.ts
+  tips: '小技巧',
+  'tips text': `Imgur上傳將產生以下格式的鏈接：![deletehash](url)。
+  [deletehash]用於刪除剛剛上傳的影像。
+  如果您的筆記會被公開，請及時在筆記中刪除deletehash。`,
+  'Imgur Settings': 'Imgur配置',
+  'Anonymous Upload': '匿名上傳',
+  'Anonymous Upload desc': '匿名上傳的檔案不會顯示在Imgur帳戶中。',
+  imgurid: '客戶端id',
+  'built-in id desc': '內建ID有每日使用限制，如果暫時失效，可以使用自己的客戶端ID以進行上傳和刪除。',
+  delete: '刪除',
+  'delete desc': '如果需要刪除Imgur上的影像，請在此處使用deletehash將其刪除。',
+  'delete btn': '刪除',
+  'delete done': '刪除成功',
+  'delete fail': '刪除失敗',
+  authenticate: '認證',
+  'auth desc': '有時需要手動重新整理以顯示認證結果。',
+  'imgur account': '目前使用者： ',
+  'auth error': '認證錯誤: ',
+  'sign in': '登錄',
+  'Sign Out': '註銷',
+  // fragment-imgurl.ts
+  'ImgURL Settings': 'ImgURL配置',
+  // fragment-smms.ts
+  'SM.MS Settings': 'SM.MS配置'
+}
