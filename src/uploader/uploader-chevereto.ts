270e4bbf3bf3529c11e2431f851a814aad582bc2
yaleiyale <wodcode@outlook.com>
2023-04-24 22:21:38 +0800

    support chevereto

--- src/uploader/uploader-chevereto.ts
+++ src/uploader/uploader-chevereto.ts
@@ -1,7 +1,8 @@
 import { request, RequestUrlParam } from 'obsidian'
 import { EmoUploader } from '../base/emo-uploader'
 import { CheveretoParms } from '../parms/parms-chevereto'
-import { getBase64 } from '../utils/file-helper'
+import { ReqFormData } from '../utils/req-formdata'
+import { CONTENT_TYPE_FORMDATA } from '../base/constants'
 
 export class CheveretoUploader extends EmoUploader {
   parms!: CheveretoParms
@@ -10,17 +11,28 @@ export class CheveretoUploader extends EmoUploader {
     this.parms = cheveretoParms
   }
 
-  async upload (file: File): Promise<string> {
-    const jsonBody = {
-      key: this.parms.required.token,
-      source: await getBase64(file)
+  // here chevereto API should be look like this: https://imgloc.com/api/1/upload
+  formatUrl (str: string): string {
+    if (!str.startsWith('https://') && !str.startsWith('http://')) {
+      str = 'https://' + str
     }
-    const form = JSON.stringify(jsonBody)
+    if (str.endsWith('/')) { // remove '/'
+      str = str.slice(0, -1)
+    }
+    return str
+  }
+
+  async upload (file: File): Promise<string> {
+    const domain = this.formatUrl(this.parms.required.domain)
+    const formData = new ReqFormData()
+    await formData.addFile('source', file)
+    const form = formData.pack()
     const req: RequestUrlParam = {
-      url: this.parms.required.domain,
+      url: domain,
       method: 'POST',
       headers: {
-        'X-API-Key': this.parms.required.token
+        'X-API-Key': this.parms.required.token,
+        'Content-Type': CONTENT_TYPE_FORMDATA
       },
       body: form
     }
